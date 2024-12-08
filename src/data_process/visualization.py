f1332932c90c0e5e27d203f0ef32b46ef27add90
shawn.kim <dun03191@gmail.com>
2024-12-08 13:54:55 -0800

    feat: update visualization code

--- src/data_process/visualization.py
+++ src/data_process/visualization.py
@@ -43,11 +43,13 @@ class Visualizer:
         apds.append(mpf.make_addplot([70]*len(df), panel=1, color='grey', linestyle='--', linewidths=0.5))
         apds.append(mpf.make_addplot([30]*len(df), panel=1, color='grey', linestyle='--', linewidths=0.5))
 
+        axes_annotations = []
+        i = 0
         # Loop through divergence data and prepare annotations
         for idx, row in divergence_df.iterrows():
             try:
                 start_datetime = pd.to_datetime(row['start_datetime'])
-                end_datetime = pd.to_datetime(row['end_datetime'])
+                end_datetime = idx
                 entry_datetime = pd.to_datetime(row['entry_datetime'])
                 previous_peak_datetime = pd.to_datetime(row['previous_peak_datetime'])
                 tp = row['TP']
@@ -106,15 +108,21 @@ class Visualizer:
                 add_lines['colors'].append('grey')
                 add_lines['linewidths'].append(1)
                 add_lines['linestyle'].append(':')
-                add_lines['panels'].append(0)  # Main panel
+                # add_lines['panels'].append(0)  # Main panel
 
                 # Add TP, SL, and Entry Price lines using hlines
-                entry_price = df.loc[entry_datetime, price_name]
+                entry_price = df.loc[entry_datetime, 'open']
                 hlines['hlines'].extend([tp, sl, entry_price])
                 hlines['colors'].extend(['g', 'r', 'b'])  # Use 'b' for entry price line
                 hlines['linestyle'].extend(['-.', '-.', '--'])
                 hlines['linewidths'].extend([1, 1, 1])
                 # hlines['panels'].extend([0, 0, 0])  # Main panel
+
+                # Add TP, SL, and entry annotations
+                axes_annotations.append((entry_datetime, entry_price, f"Entry{i+1}: {entry_price:.2f}", 'blue'))
+                axes_annotations.append((end_datetime, tp, f"TP{i+1}: {tp:.2f}", 'green'))
+                axes_annotations.append((end_datetime, sl, f"SL{i+1}: {sl:.2f}", 'red'))
+                i += 1
 
             except KeyError as e:
                 print(f"KeyError: {e} for divergence at index {idx}. Skipping.")
@@ -139,22 +147,33 @@ class Visualizer:
         # Plot the chart
         fig, axes = mpf.plot(ohlc_df, **kwargs, returnfig=True)
 
-        # Optional: Add TP and SL annotations
-        # for idx, row in divergence_df.iterrows():
-        #     try:
-        #         tp = row['TP']
-        #         sl = row['SL']
-        #         divergence_type = row['divergence']
-        #         color = 'green' if divergence_type == 'Bullish Divergence' else 'red'
 
-        #         # Add TP and SL annotations
-        #         axes[0].text(df.index[-1], tp, f'TP: {tp:.2f}', color='green', fontsize=10, verticalalignment='bottom')
-        #         axes[0].text(df.index[-1], sl, f'SL: {sl:.2f}', color='red', fontsize=10, verticalalignment='top')
-        #     except Exception as e:
-        #         continue
+        # Add annotations for TP, SL, and Entry
+        for _, value, text, color in axes_annotations:
+            axes[0].annotate(
+                text, 
+                xy=(_, value),
+                xytext=(_, value),
+                arrowprops=dict(facecolor=color, shrink=0.05),
+                fontsize=8, color=color
+            )
 
         if save_fig:
             fig.savefig(f"{title.replace('/', '_')}.png")
             fig.close()
         else:
             fig.show()
+
+
+if __name__ == "__main__":
+    PROJECT_PATH = "/Users/shawn/Documents/personal/rsi_divergence_detector"
+    import sys
+    import pandas as pd
+    sys.path.append(f"{PROJECT_PATH}")
+    df_15m_filter = pd.read_pickle(f"{PROJECT_PATH}/data/df_15m_filter_test.pickle")
+    df_15m_filter[-5:]
+    divergence_data = pd.read_pickle(f"{PROJECT_PATH}/data/divergence_data")
+    divergence_15m = divergence_data['15m']
+    divergence_15m[-2:]
+    visualizer = Visualizer
+    visualizer.plot_divergence(df_15m_filter, divergence_15m[-2:])
